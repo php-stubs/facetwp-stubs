f942024684943c3b64001f1bcf7a5a55a4679c49
Jaime Martínez <info@jaimemartinez.nl>
2025-08-27 13:45:27 +0200

    simplify and cleanup release bash script

--- release-latest-versions.sh
+++ release-latest-versions.sh
@@ -1,41 +1,26 @@
 #!/bin/bash
 #
-# Download ACF PRO releases and generate stubs for each one.
+# Manually download FacetWP release and generate stubs for each one
 #
-# apt-get install wget xmlstarlet unzip git
-#
-# Start: ACF_PRO_KEY="Your ACF PRO license key here" ./release-latest-versions.sh
+# ./release-latest-versions.sh
 
-get_version()
-{
-    # rm -f -r source/facetwp
+rm -f -r source/facetwp
+unzip -qq source/facetwp.zip -d source
+test -d "source/facetwp"
 
-    # # Download page, convert to XML, parse release versions
-    # wget "https://github.com/wordpress-premium/facetwp/archive/refs/heads/main.zip" -O  source/facetwp.zip
-    # unzip source/facetwp.zip -d source
-    # mv source/facetwp-main source/facetwp
+VERSION=$(php -f get_version.php);
 
-    test -d "source/facetwp"
+echo "Releasing ${VERSION} version ..."
 
-    rm -rf "source/facetwp/facetwp"
-    rm -rf "source/facetwp/FacetWP Addons"
-    echo $(php -f get_version.php);
-}
+if git rev-parse "refs/tags/v${VERSION}" >/dev/null 2>&1; then
+    echo "Tag exists!"
+    continue
+fi
 
-while read -r VERSION; do
+# Generate stubs
+echo "Generating stubs ..."
+./generate.sh
 
-    echo "Releasing ${VERSION} version ..."
-
-    if git rev-parse "refs/tags/v${VERSION}" >/dev/null 2>&1; then
-        echo "Tag exists!"
-        continue
-    fi
-
-    # Generate stubs
-    echo "Generating stubs ..."
-    ./generate.sh
-
-    # # Tag version
-    git commit facetwp-stubs.php -m "Generate stubs for FacetWP ${VERSION}"
-    git tag "v${VERSION}"
-done < <(get_version | tac)
+# # Tag version
+git commit facetwp-stubs.php -m "Generate stubs for FacetWP ${VERSION}"
+git tag "v${VERSION}"
